cfb62d417854317fe9a5f66431a1aea411e85e39
Igor dos Santos <igr.exe@gmail.com>
2013-10-22 16:13:03 -0200

    classe token
    adicionado classe token
    mostrando coluna que se encontra o erro

--- src/gramatica.py
+++ src/gramatica.py
@@ -14,7 +14,7 @@ class Analise(object):
         for count in xrange(0, len(expressao)):
             i = expressao[count]
             if self._estado == 0:
-                if self.validar_alfabeto(i, 'Caracter inválido no estado 0'): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inválido no estado 0 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self._estado = 1
@@ -25,38 +25,38 @@ class Analise(object):
                         lexema += i
                         self._estado = 4
                     else:
-                        raise Exception('Esse caracter não tem próximo estado.')
+                        raise Exception('Sequência de caracter inválido. : Coluna %d' % count)
             elif self._estado == 1:
-                if self.validar_alfabeto(i, 'Caracter inválido no estado 1'): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inválido no estado 1 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self.final(count, len(expressao) - 1, 1, lexema) # verifica estado final
                     elif self._operadores.find(i) != -1:
-                        self._tokens.append(lexema)
+                        self._tokens.append(Token(lexema, 'numero'))
                         lexema = ''
-                        self._tokens.append(i)
+                        self._tokens.append(Token(i, 'operador'))
                         self._estado = 2
                     else:
-                        raise Exception('Esse caracter não tem próximo estado.')
+                        raise Exception('Sequência de caracter inválido. : Coluna %d' % count)
             elif self._estado == 2:
-                if self.validar_alfabeto(i, 'Caracter inválido no estado 2'): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inválido no estado 2 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self.final(count, len(expressao) - 1, 1, lexema) # verifica estado final
                     elif i == '(':
-                        self._tokens.append(i)
+                        self._tokens.append(Token(i, 'parenteses'))
                         self._estado = 4
                     else:
-                        raise Exception('Esse caracter não tem próximo estado.')
+                        raise Exception('Sequência de caracter inválido. : Coluna %d' % count)
             elif self._estado == 3:
-                if self.validar_alfabeto(i, 'Caracter inválido no estado 3'): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inválido no estado 3 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self.final(count, len(expressao) - 1, 1, lexema) # verifica estado final
                     else:
-                        raise Exception('Esse caracter não tem próximo estado.')
+                        raise Exception('Sequência de caracter inválido. : Coluna %d' % count)
             elif self._estado == 4:
-                if self.validar_alfabeto(i, 'Caracter inválido no estado 4'): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inválido no estado 4 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self._estado = 5
@@ -64,38 +64,38 @@ class Analise(object):
                         lexema += i
                         self._estado = 7
                     else:
-                        raise Exception('Esse caracter não tem próximo estado.')
+                        raise Exception('Sequência de caracter inválido. : Coluna %d' % count)
             elif self._estado == 5:
-                if self.validar_alfabeto(i, 'Caracter inválido no estado 5'): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inválido no estado 5 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self._estado = 5
                     elif self._operadores.find(i) != -1:
-                        self._tokens.append(lexema)
+                        self._tokens.append(Token(lexema, 'numero'))
                         lexema = ''
-                        self._tokens.append(i)
+                        self._tokens.append(Token(i, 'operador'))
                         self._estado = 4
                     elif i == ')':
-                        self._tokens.append(lexema) # adicionado lexema antes, pois se trata de parenteses
+                        self._tokens.append(Token(lexema, 'numero')) # adicionado lexema antes, pois se trata de parenteses
                         lexema = '' # esvazia lexema, para não ser adicionada no estado final
                         self.final(count, len(expressao) - 1, 6, lexema) # verifica estado final
-                        self._tokens.append(i)
+                        self._tokens.append(Token(i, 'parenteses'))
                     else:
-                        raise Exception('Esse caracter não tem próximo estado.')
+                        raise Exception('Sequência de caracter inválido. : Coluna %d' % count)
             elif self._estado == 6:
-                if self.validar_alfabeto(i, 'Caracter inválido no estado 6'): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inválido no estado 6 : Coluna %d' % count): # verificar caracter valido
                     if self._operadores.find(i) != -1:
-                        self._tokens.append(i)
+                        self._tokens.append(Token(i, 'operador'))
                         self._estado = 2
                     else:
-                        raise Exception('Esse caracter não tem próximo estado.')
+                        raise Exception('Sequência de caracter inválido. : Coluna %d' % count)
             elif self._estado == 7:
-                if self.validar_alfabeto(i, 'Caracter inválido no estado 7'): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inválido no estado 7 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self._estado = 5
                     else:
-                        raise Exception('Esse caracter não tem próximo estado.')
+                        raise Exception('Sequência de caracter inválido. : Coluna %d' % count)
                     
     def validar_alfabeto(self, char, msg):
         if self._alfabeto.find(char) != -1:
@@ -106,7 +106,7 @@ class Analise(object):
     def final(self, count, condicao, estado, lexema):
         if count == condicao:
             if lexema != '':
-                self._tokens.append(lexema)
+                self._tokens.append(Token(lexema, 'numero'))
         else:
             self._estado = estado
    
@@ -118,14 +118,15 @@ class Analise(object):
 class Token(object):
 
     def __init__(self, lexema, token):
-        self.token = token
         self.lexema = lexema
+        self.token = token
 
 def main():
     expressao = '-3*3+(+3-5)/3'
     analise = Analise()
     analise.lexica(expressao)
-    print analise.get_tokens()
+    for i in analise.get_tokens():
+        print "Lexema: %s\tToken: %s" % (i.lexema, i.token)
 
 if __name__ == '__main__':
     main()
