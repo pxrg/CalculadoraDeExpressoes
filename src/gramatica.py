241543b02c1eddcb4123cf2e02e214de904836fd
igr-santos <igr.exe@gmail.com>
2013-10-27 14:02:13 -0200

    gramatica fazendo lexica separada
    faltando apenas validar tokens na gramatica

--- src/gramatica.py
+++ src/gramatica.py
@@ -11,10 +11,34 @@ class Analise(object):
 
     def lexica(self, expressao):
         lexema = ''
+        valido = True
+        for pos in xrange(0, len(expressao)):
+            char = expressao[pos]
+            if self.validar_alfabeto(char):
+                if char.isdigit() or (self._status.find(char) != -1 and valido):
+                    lexema += char
+                    valido = False
+                    if pos == (len(expressao)-1):
+                        self._tokens.append(Token(lexema, 'numero'))
+                elif self._operadores.find(char) != -1:
+                    self._tokens.append(Token(lexema, 'numero'))
+                    lexema = ''
+                    self._tokens.append(Token(char, 'operador'))
+                elif char == '(' or ')':
+                    if lexema != '':
+                        self._tokens.append(Token(lexema, 'numero'))
+                    else:
+                        valido = True
+                    self._tokens.append(Token(char, 'parenteses'))
+            else:
+                raise Exception('Caracter inv√°lido na coluna %d' % pos)
+
+    def lexica_segundo(self, expressao):
+        lexema = ''
         for count in xrange(0, len(expressao)):
             i = expressao[count]
             if self._estado == 0:
-                if self.validar_alfabeto(i, 'Caracter inv·lido no estado 0 : Coluna %d' % count): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inv√°lido no estado 0 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self._estado = 1
@@ -25,9 +49,9 @@ class Analise(object):
                         lexema += i
                         self._estado = 4
                     else:
-                        raise Exception('SequÍncia de caracter inv·lido. : Coluna %d' % count)
+                        raise Exception('Sequ√™ncia de caracter inv√°lido. : Coluna %d' % count)
             elif self._estado == 1:
-                if self.validar_alfabeto(i, 'Caracter inv·lido no estado 1 : Coluna %d' % count): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inv√°lido no estado 1 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self.final(count, len(expressao) - 1, 1, lexema) # verifica estado final
@@ -37,9 +61,9 @@ class Analise(object):
                         self._tokens.append(Token(i, 'operador'))
                         self._estado = 2
                     else:
-                        raise Exception('SequÍncia de caracter inv·lido. : Coluna %d' % count)
+                        raise Exception('Sequ√™ncia de caracter inv√°lido. : Coluna %d' % count)
             elif self._estado == 2:
-                if self.validar_alfabeto(i, 'Caracter inv·lido no estado 2 : Coluna %d' % count): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inv√°lido no estado 2 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self.final(count, len(expressao) - 1, 1, lexema) # verifica estado final
@@ -47,16 +71,16 @@ class Analise(object):
                         self._tokens.append(Token(i, 'parenteses'))
                         self._estado = 4
                     else:
-                        raise Exception('SequÍncia de caracter inv·lido. : Coluna %d' % count)
+                        raise Exception('Sequ√™ncia de caracter inv√°lido. : Coluna %d' % count)
             elif self._estado == 3:
-                if self.validar_alfabeto(i, 'Caracter inv·lido no estado 3 : Coluna %d' % count): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inv√°lido no estado 3 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self.final(count, len(expressao) - 1, 1, lexema) # verifica estado final
                     else:
-                        raise Exception('SequÍncia de caracter inv·lido. : Coluna %d' % count)
+                        raise Exception('Sequ√™ncia de caracter inv√°lido. : Coluna %d' % count)
             elif self._estado == 4:
-                if self.validar_alfabeto(i, 'Caracter inv·lido no estado 4 : Coluna %d' % count): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inv√°lido no estado 4 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self._estado = 5
@@ -64,9 +88,9 @@ class Analise(object):
                         lexema += i
                         self._estado = 7
                     else:
-                        raise Exception('SequÍncia de caracter inv·lido. : Coluna %d' % count)
+                        raise Exception('Sequ√™ncia de caracter inv√°lido. : Coluna %d' % count)
             elif self._estado == 5:
-                if self.validar_alfabeto(i, 'Caracter inv·lido no estado 5 : Coluna %d' % count): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inv√°lido no estado 5 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self._estado = 5
@@ -77,31 +101,31 @@ class Analise(object):
                         self._estado = 4
                     elif i == ')':
                         self._tokens.append(Token(lexema, 'numero')) # adicionado lexema antes, pois se trata de parenteses
-                        lexema = '' # esvazia lexema, para n„o ser adicionada no estado final
+                        lexema = '' # esvazia lexema, para n√£o ser adicionada no estado final
                         self.final(count, len(expressao) - 1, 6, lexema) # verifica estado final
                         self._tokens.append(Token(i, 'parenteses'))
                     else:
-                        raise Exception('SequÍncia de caracter inv·lido. : Coluna %d' % count)
+                        raise Exception('Sequ√™ncia de caracter inv√°lido. : Coluna %d' % count)
             elif self._estado == 6:
-                if self.validar_alfabeto(i, 'Caracter inv·lido no estado 6 : Coluna %d' % count): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inv√°lido no estado 6 : Coluna %d' % count): # verificar caracter valido
                     if self._operadores.find(i) != -1:
                         self._tokens.append(Token(i, 'operador'))
                         self._estado = 2
                     else:
-                        raise Exception('SequÍncia de caracter inv·lido. : Coluna %d' % count)
+                        raise Exception('Sequ√™ncia de caracter inv√°lido. : Coluna %d' % count)
             elif self._estado == 7:
-                if self.validar_alfabeto(i, 'Caracter inv·lido no estado 7 : Coluna %d' % count): # verificar caracter valido
+                if self.validar_alfabeto(i, 'Caracter inv√°lido no estado 7 : Coluna %d' % count): # verificar caracter valido
                     if i.isdigit():
                         lexema += i
                         self._estado = 5
                     else:
-                        raise Exception('SequÍncia de caracter inv·lido. : Coluna %d' % count)
+                        raise Exception('Sequ√™ncia de caracter inv√°lido. : Coluna %d' % count)
                     
-    def validar_alfabeto(self, char, msg):
+    def validar_alfabeto(self, char):
         if self._alfabeto.find(char) != -1:
             return True
         else:
-            raise Exception(msg)
+            raise False
 
     def final(self, count, condicao, estado, lexema):
         if count == condicao:
@@ -123,6 +147,7 @@ class Token(object):
 
 def main():
     expressao = '-3*3+(+3-5)/3'
+    expressao = '-3*3+(+3-5)/3+)'
     analise = Analise()
     analise.lexica(expressao)
     for i in analise.get_tokens():
